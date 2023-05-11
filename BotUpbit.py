39e5b245056aced6644b439234c1be03b5e722ee
ckgudcjf <ckgudcjf@gmail.com>
2023-05-11 21:25:38 +0900

    ing

--- BotUpbit.py
+++ BotUpbit.py
@@ -55,7 +55,6 @@ class BotCoin():
         prc_ttl, prc_lmt, _, bal_lst  = self.get_balance_info(self.q_l)
         self.b_l = list(set(self.q_l + bal_lst))
         self.prc_ttl = prc_ttl if prc_ttl < self.const_up else self.const_up
-        self.prc_ttl = 10000000
         self.prc_lmt = prc_lmt if prc_ttl < self.const_up else prc_lmt - self.const_up
         prc_max = self.prc_ttl / len(self.q_l)
         self.prc_max = prc_max if prc_max > self.const_dn else self.const_dn
