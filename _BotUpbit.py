dfa26a6995183357a90ce2ce0d221de38c293c0f
ckgudcjf <ckgudcjf@gmail.com>
2023-05-16 15:00:14 +0900

    ing

--- _BotUpbit.py
+++ _BotUpbit.py
@@ -112,7 +112,7 @@ class BotUpbit():
             is_symbol_bal = symbol in bal_lst
             is_symbol_obj = symbol in obj_lst
 
-            df = MACD(RSI(VO(self.gen_ubt_df(symbol, 'minute5', 100))))
+            df = MACD(RSI(VO(self.gen_ubt_df(symbol, 'minute5', 80))))
 
             if (not (df is None)):
                 
